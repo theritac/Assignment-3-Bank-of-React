a62081dbcb86c6ad0b69665319e2949645945c52
hninl23 <h.lwin0440@gmail.com>
2023-10-26 16:54:06 -0400

    updated Debit func

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,28 +4,28 @@ src/components/Debits.js
 The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Debits = (props) => {
   // Create the list of Debit items
-    const submissionDebit = (event) => {
-      event.preventDefault()
+  const submissionDebit = (event) => {
+    event.preventDefault()
 
-      const newDebit = {
-        description: event.target.description.value,
-        amount: parseFloat(event.target.amount.value),
-        date: new Date().toISOString()
-      }
-
-      props.addDebit(newDebit);
-      event.target.reset()
+    const newDebit = {
+      description: event.target.description.value,
+      amount: parseFloat(event.target.amount.value).toFixed(2),
+      date: new Date().toISOString()
     }
-  
+
+    props.addDebit(newDebit);
+    event.target.reset()
+  }
+
   let debitsView = () => {
     const { debits } = props;
-    return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debit.date.slice(0,10);
-      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+    return debits.map((debits) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      let date = debits.date.slice(0, 10);
+      return <li key={debits.id}>{debits.amount} {debits.description} {date}</li>
     });
   }
   // Render the list of Debit items and a form to input new Debit item
@@ -37,13 +37,12 @@ const Debits = (props) => {
 
       <form onSubmit={submissionDebit}>
         <input type="text" name="description" />
-        <input type="number" name="amount" />
+        <input type="number" step="any" name="amount" />
         <button type="submit">Add Debit</button>
       </form>
-      <br/>
+      <br />
       <Link to="/">Return to Home</Link>
     </div>
   );
 }
-
 export default Debits;
