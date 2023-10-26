c1eb3e2ebe903259481e76920545a31cb889ea37
HninL <100245412+hninl23@users.noreply.github.com>
2023-10-26 13:47:13 -0400

    Revert "added submission func to submit the form"

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,43 +4,17 @@ src/components/Debits.js
 The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import { Link } from 'react-router-dom';
-
-// //add debit card information
-// const addDebit = () => {
-//   this.setState(state => {
-//     return {
-//       description: state.
-//     }
-//   })
-// }
+import {Link} from 'react-router-dom';
 
 const Debits = (props) => {
   // Create the list of Debit items
-
-  const submissionDebit = (event) => {
-    event.preventDefault()
-
-    const newDebit = {
-      description: event.target.description.value,
-      amount: parseFloat(event.target.amount.value),
-      date: new Date().toISOString()
-    }
-
-    props.addDebit(newDebit);
-    event.target.reset()
-  }
-
   let debitsView = () => {
     const { debits } = props;
-
     return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debit.date.slice(0, 10);
+      let date = debit.date.slice(0,10);
       return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
     });
   }
-
-
   // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
@@ -48,13 +22,12 @@ const Debits = (props) => {
 
       {debitsView()}
 
-      <form onSubmit={submissionDebit}>
+      <form onSubmit={props.addDebit}>
         <input type="text" name="description" />
         <input type="number" name="amount" />
-
         <button type="submit">Add Debit</button>
       </form>
-      <br />
+      <br/>
       <Link to="/">Return to Home</Link>
     </div>
   );
