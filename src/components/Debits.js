d7316f326ac8b9d0c2a442d9f20c40055bd8202f
hninl23 <h.lwin0440@gmail.com>
2023-10-29 21:44:24 -0400

    subtract from balance in Debit

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -5,34 +5,40 @@ The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import { Link } from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Debits = (props) => {
+  const { debits, balance, updateAccountBalance } = props;
   // Create the list of Debit items
+  let newBalance = 0;
+
   const submissionDebit = (event) => {
     event.preventDefault()
-
+    newBalance = (balance - parseFloat(event.target.amount.value).toFixed(2));
     const newDebit = {
+      id: debits.length + 1,
       description: event.target.description.value,
       amount: parseFloat(event.target.amount.value).toFixed(2),
       date: new Date().toISOString()
     }
 
+    updateAccountBalance(newBalance);
     props.addDebit(newDebit);
     event.target.reset()
   }
 
   let debitsView = () => {
-    const { debits } = props;
     return debits.map((debits) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
       let date = debits.date.slice(0, 10);
       return <li key={debits.id}>{debits.amount} {debits.description} {date}</li>
     });
   }
+
   // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
       <h1>Debits</h1>
-
+      <AccountBalance accountBalance={balance} />
       {debitsView()}
 
       <form onSubmit={submissionDebit}>
@@ -45,4 +51,5 @@ const Debits = (props) => {
     </div>
   );
 }
+
 export default Debits;
